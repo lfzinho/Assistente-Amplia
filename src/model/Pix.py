6e5505e63e711a24867f20940ffb9709ad0e945d
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-11-19 17:59:53 -0300

    Testa herança da classe Pix

--- src/model/Pix.py
+++ src/model/Pix.py
@@ -1,6 +1,17 @@
 from model.MetodoPagamento import MetodoPagamento
 
+# testa_informacoes_construtor
+# class Pix:
+#     def __init__(self, chave:str, tipo:str) -> None:
+#         self.chave = chave
+#         self.tipo = tipo
+
+
+# testa_heranca_metodo_pagamento
 class Pix(MetodoPagamento):
     def __init__(self, chave:str, tipo:str) -> None:
         self.chave = chave
         self.tipo = tipo
+
+    def visualizar_informacoes(self):
+        pass
--- src/test/TestaPix.py
+++ src/test/TestaPix.py
@@ -4,13 +4,20 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import unittest
+from model.MetodoPagamento import MetodoPagamento
 from model.Pix import Pix
 
 class TestaPix(unittest.TestCase):
-    """Testa o construtor da classe Pix conferindo se os atributos foram setados corretamente"""
-    pix = Pix('12345678910', 'cpf')
-    self.assertEqual(pix.chave, '12345678910')
-    self.assertEqual(pix.tipo, 'cpf')
+
+    def testa_informacoes_construtor(self):
+        """Testa o construtor da classe Pix conferindo se os atributos foram setados corretamente"""
+        pix = Pix('12345678910', 'cpf')
+        self.assertEqual(pix.chave, '12345678910')
+        self.assertEqual(pix.tipo, 'cpf')
+
+    def testa_heranca_metodo_pagamento(self):
+        """Testa se a classe Pix herda de MetodoPagamento"""
+        self.assertTrue(issubclass(Pix, MetodoPagamento))
 
 if __name__ == '__main__':
     unittest.main()

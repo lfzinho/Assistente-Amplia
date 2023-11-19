af9bfa061773b6f63f3d7b304b324b193c42d5b0
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-11-19 17:53:23 -0300

    Cria a classe de teste para Metodo de Pagamento

---
+++ src/test/TestaMetodoPagamento.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import unittest
+from model import MetodoPagamento
+
+class TestaMetodoPagamento(unittest.TestCase):
+    

67d712a55a59b70ac1a6765027890d610123ac20
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-11-22 20:17:39 -0300

    Testa setters

--- src/models/Pessoa.py
+++ src/models/Pessoa.py
@@ -20,6 +20,43 @@ from src.models.MetodoPagamento import MetodoPagamento
 #         self.metodo_pagamento = metodo_pagamento
 
 # testa_getters
+# class Pessoa:
+#     """Classe que representa uma pessoa"""
+#     def __init__(
+#             self, 
+#             nome:str, 
+#             data_nascimento:date,
+#             cpf:str,
+#             endereco:str,
+#             metodo_pagamento:Type[MetodoPagamento]
+#     ) -> None:
+#         self._nome = nome
+#         self._data_nascimento = data_nascimento
+#         self._cpf = cpf
+#         self._endereco = endereco
+#         self._metodo_pagamento = metodo_pagamento
+
+#     @property
+#     def nome(self) -> str:
+#         return self._nome
+    
+#     @property
+#     def data_nascimento(self) -> date:
+#         return self._data_nascimento
+    
+#     @property
+#     def cpf(self) -> str:
+#         return self._cpf
+    
+#     @property
+#     def endereco(self) -> str:
+#         return self._endereco
+    
+#     @property
+#     def metodo_pagamento(self) -> Type[MetodoPagamento]:
+#         return self._metodo_pagamento
+    
+# testa_setters
 class Pessoa:
     """Classe que representa uma pessoa"""
     def __init__(
@@ -40,19 +77,38 @@ class Pessoa:
     def nome(self) -> str:
         return self._nome
     
+    @nome.setter
+    def nome(self, nome: str):
+        self._nome = nome
+    
     @property
     def data_nascimento(self) -> date:
         return self._data_nascimento
     
+    @data_nascimento.setter
+    def data_nascimento(self, data_nascimento: date):
+        self._data_nascimento = data_nascimento
+
     @property
     def cpf(self) -> str:
         return self._cpf
+    
+    @cpf.setter
+    def cpf(self, cpf: str):
+        self._cpf = cpf
     
     @property
     def endereco(self) -> str:
         return self._endereco
     
+    @endereco.setter
+    def endereco(self, endereco: str):
+        self._endereco = endereco
+
     @property
     def metodo_pagamento(self) -> Type[MetodoPagamento]:
         return self._metodo_pagamento
     
+    @metodo_pagamento.setter
+    def metodo_pagamento(self, metodo_pagamento: Type[MetodoPagamento]):
+        self._metodo_pagamento = metodo_pagamento
--- tests/test_models/TestaPessoa.py
+++ tests/test_models/TestaPessoa.py
@@ -5,6 +5,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 import unittest
 from src.models.Pessoa import Pessoa
 from src.models.Pix import Pix
+from src.models.ContaBancaria import ContaBancaria
 
 
 class TestaPessoa(unittest.TestCase):
@@ -37,6 +38,26 @@ class TestaPessoa(unittest.TestCase):
         self.assertEqual(pessoa._cpf, '123.456.789-00')
         self.assertEqual(pessoa._endereco, 'Rua 1, 123')
         self.assertEqual(pessoa._metodo_pagamento.visualizar_informacoes(), 'Chave: 12345678900\nTipo: cpf')
+
+    def testa_setters(self):
+        """Testa os setters da classe Pessoa"""
+        pessoa = Pessoa(
+            nome='João',
+            data_nascimento='1990-01-01',
+            cpf='123.456.789-00',
+            endereco='Rua 1, 123',
+            metodo_pagamento=Pix(chave='12345678900', tipo='cpf')
+        )
+        pessoa.nome = 'Maria'
+        pessoa.data_nascimento = '1990-02-02'
+        pessoa.cpf = '987.654.321-00'
+        pessoa.endereco = 'Rua 2, 321'
+        pessoa.metodo_pagamento = ContaBancaria(banco='Banco 1', agencia='123', nro_conta='123456')
+        self.assertEqual(pessoa._nome, 'Maria')
+        self.assertEqual(pessoa._data_nascimento, '1990-02-02')
+        self.assertEqual(pessoa._cpf, '987.654.321-00')
+        self.assertEqual(pessoa._endereco, 'Rua 2, 321')
+        self.assertEqual(pessoa._metodo_pagamento.visualizar_informacoes(), 'Banco: Banco 1\nAgência: 123\nNúmero da conta: 123456')
 
 if __name__ == '__main__':
     unittest.main()

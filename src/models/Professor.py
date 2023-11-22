65f197979ccbeac7c76325d586bb434a39a2c794
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-11-22 20:46:40 -0300

    Adiciona o construtor de Professor

--- src/models/Professor.py
+++ src/models/Professor.py
@@ -8,3 +8,18 @@ from src.models.MetodoPagamento import MetodoPagamento
 # testa_heraca_pessoa
 class Professor(Pessoa):
     pass
+
+# testa_construtor
+class Professor(Pessoa):
+
+    def __init__(
+            self,
+            nome: str,
+            data_nascimento: date,
+            cpf: str,
+            endereco: str,
+            metodo_pagamento: type[MetodoPagamento]
+        ) -> None:
+
+        super().__init__(nome, data_nascimento, cpf, endereco, metodo_pagamento)
+
--- tests/test_models/TestaProfessor.py
+++ tests/test_models/TestaProfessor.py
@@ -17,3 +17,29 @@ class TestaProfessor(unittest.TestCase):
     def testa_heranca_pessoa(self):
         """Testa se a classe Aluno herda de Pessoa"""
         self.assertTrue(issubclass(Professor, Pessoa))
+
+    def testa_construtor(self):
+        """Testa se o construtor da classe Professor funciona"""
+        professor = Professor(
+            nome='João',
+            data_nascimento='1999-01-01',
+            cpf='12345678901',
+            endereco='Rua 1',
+            metodo_pagamento=Pix(
+                chave='12345678901',
+                tipo='cpf'
+            )
+        )
+        # testa se a instância é da classe Professor
+        self.assertIsInstance(professor, Professor)
+        self.assertIsInstance(professor, Pessoa)
+        # testa se os atributos foram definidos corretamente
+        self.assertEqual(professor.nome, 'João')
+        self.assertEqual(professor.data_nascimento, '1999-01-01')
+        self.assertEqual(professor.cpf, '12345678901')
+        self.assertEqual(professor.endereco, 'Rua 1')
+        self.assertIsInstance(professor.metodo_pagamento, Pix)
+
+
+if __name__ == '__main__':
+    unittest.main()

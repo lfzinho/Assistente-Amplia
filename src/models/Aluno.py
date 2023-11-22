7b3dffe993383e8d865353f3c33d3148d90ac39a
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-11-22 20:29:58 -0300

    Cria Aluno.py

---
+++ src/models/Aluno.py
@@ -0,0 +1,14 @@
+from typing import Type
+
+from src.models.Pessoa import Pessoa
+
+from datetime import date
+from src.models.MetodoPagamento import MetodoPagamento
+
+
+# testa_heraca_pessoa
+# class Aluno(Pessoa):
+#     pass
+
+class Aluno(Pessoa):
+    pass
--- tests/test_models/TestaAluno.py
+++ tests/test_models/TestaAluno.py
@@ -5,3 +5,19 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 import unittest
 
 from src.models.Aluno import Aluno
+
+from src.models.Pessoa import Pessoa
+
+from src.models.Pix import Pix
+from src.models.ContaBancaria import ContaBancaria
+
+
+class TestaAluno(unittest.TestCase):
+
+    def testa_heranca_pessoa(self):
+        """Testa se a classe Aluno herda de Pessoa"""
+        self.assertTrue(issubclass(Aluno, Pessoa))
+
+
+if __name__ == '__main__':
+    unittest.main()

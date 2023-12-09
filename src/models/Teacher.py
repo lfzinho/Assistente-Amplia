13d74fbf24bf62e114594fb3de8a3964aaec53bc
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-12-09 17:03:06 -0300

    Create Teacher

---
+++ src/models/Teacher.py
@@ -0,0 +1,46 @@
+from datetime import date
+from typing import Optional, Type
+
+from src.models.PaymentMethod import PaymentMethod
+from src.models.Beneficiary import Beneficiary
+
+
+class Teacher(Beneficiary):
+    """Classe que representa um professor."""
+    def __init__(
+        self,
+        name: str,
+        email: str,
+        cpf: str,
+        address: str,
+        payment_method: Type[PaymentMethod],
+        birth_date: date,
+        admission_date: date,
+        transport_cost: float,
+        transport_description: str,
+        theme: str,
+        exit_date: Optional[date] = None,
+    ) -> None:
+        super().__init__(
+            name=name,
+            email=email,
+            cpf=cpf,
+            address=address,
+            payment_method=payment_method,
+            birth_date=birth_date,
+            admission_date=admission_date,
+            transport_cost=transport_cost,
+            transport_description=transport_description,
+            exit_date=exit_date
+        )
+        self.theme = theme
+
+    @property
+    def theme(self) -> str:
+        """Getter do tema."""
+        return self._theme
+
+    @theme.setter
+    def theme(self, theme: str) -> None:
+        """Setter do tema."""
+        self._theme = theme

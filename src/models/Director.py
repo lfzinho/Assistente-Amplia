6318412bcba4d3a7cae50d121bc60f70f93e4578
viniciushedler <viniciushedler@gmail.com>
2023-12-09 19:33:39 -0300

    refactor: Director

--- src/models/Director.py
+++ src/models/Director.py
@@ -6,20 +6,84 @@ from src.models.Person import Person
 from src.models.PaymentMethod import PaymentMethod
 
 
+# class Director(Analyst):
+#     def __init__(self, name, email, cpf, address, payment_method,
+#         birth_date, admission_date, occupation_area,
+#         office_admission_date, office_exit_date=None, exit_date=None):
+#         super().__init__(name, email, cpf, address, payment_method,
+#         birth_date, admission_date, occupation_area, exit_date)
+#         # Checa admission_date < office_admission_date
+#         if office_admission_date < admission_date:
+#             raise ValueError("""A data de promoção não pode ser anterior
+#                 à data de admissão""")
+#         # Checa office_exit_date < office_admission_date
+#         if office_exit_date is not None:
+#             if office_exit_date < office_admission_date:
+#                 raise ValueError("""A data de saída do cargo não pode
+#                     ser anterior à data de entrada""")
+#         self.office_admission_date = office_admission_date
+#         self.office_exit_date = office_exit_date
+
+
 class Director(Analyst):
-    def __init__(self, name, email, cpf, address, payment_method,
-        birth_date, admission_date, occupation_area,
-        office_admission_date, office_exit_date=None, exit_date=None):
-        super().__init__(name, email, cpf, address, payment_method,
-        birth_date, admission_date, occupation_area, exit_date)
-        # Checa admission_date < office_admission_date
-        if office_admission_date < admission_date:
-            raise ValueError("""A data de promoção não pode ser anterior
-                à data de admissão""")
-        # Checa office_exit_date < office_admission_date
-        if office_exit_date is not None:
-            if office_exit_date < office_admission_date:
-                raise ValueError("""A data de saída do cargo não pode
-                    ser anterior à data de entrada""")
+    def __init__(
+        self,
+        name: str,
+        email: str,
+        cpf: str,
+        address: str,
+        payment_method: Type[PaymentMethod],
+        birth_date: date,
+        admission_date: date,
+        occupation_area: Literal['Administrativo Financeiro',
+        'Recursos Humanos', 'Pedagógico', 'Marketing'],
+        office_admission_date: date,
+        office_exit_date: Optional[date]=None,
+        exit_date: Optional[date] = None,
+    ) -> None:
+        super().__init__(
+            name,
+            email,
+            cpf,
+            address,
+            payment_method,
+            birth_date,
+            admission_date,
+            occupation_area,
+            exit_date,
+        )
         self.office_admission_date = office_admission_date
         self.office_exit_date = office_exit_date
+
+    @property
+    def office_admission_date(self) -> str:
+        """Getter da data de promoção do diretor"""
+        return self._occupation_area
+
+    @office_admission_date.setter
+    def office_admission_date(
+        self,
+        office_admission_date: date
+    ) -> None:
+        """Setter da data de promoção do diretor"""
+        if office_admission_date < self.admission_date:
+            raise ValueError("""A data de promoção não pode ser anterior
+                à data de admissão""")
+        self._occupation_area = office_admission_date
+
+    @property
+    def office_exit_date(self) -> str:
+        """Getter da data de saída do cargo do diretor"""
+        return self._occupation_area
+
+    @office_exit_date.setter
+    def office_exit_date(
+        self,
+        office_exit_date: date
+    ) -> None:
+        """Setter da data de saída do cargo do diretor"""
+        if office_exit_date is not None:
+            if office_exit_date < self.office_admission_date:
+                raise ValueError("""A data de saída do cargo não pode
+                    ser anterior à data de entrada""")
+        self._occupation_area = office_exit_date

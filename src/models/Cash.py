9aefb7e3b6bfd1e75d346c436c377bbe977b32e0
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-12-09 18:53:08 -0300

    style: add class docstrings

--- src/models/Cash.py
+++ src/models/Cash.py
@@ -22,10 +22,34 @@
 
 
 class Cash:
+    """Classe que armazena o saldo da instituição."""
     def __init__(self, saved_amount: float) -> None:
+        """
+        Construtor da classe Cash.
+
+        Parâmetros
+        ----------
+        saved_amount: float
+            Saldo da instituição.
+        """
         self.saved_amount = saved_amount
 
     def make_payment(self, amount: float) -> None:
+        """
+        Método para fazer pagamentos.
+
+        Parâmetros
+        ----------
+        amount: float
+            Quantia a ser paga.
+
+        Raises
+        ------
+        ValueError
+            Se a quantia for negativa ou zero.
+        ValueError
+            Se a quantia for maior que o saldo.
+        """
         if amount <= 0.0:
             raise ValueError("A quantia não pode ser negativa ou zero.")
         if amount > self.saved_amount:

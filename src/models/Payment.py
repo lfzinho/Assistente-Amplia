caa53a598e33af1e1b9e3ed347060e2f940d0f13
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-12-09 19:12:55 -0300

    feat: add Payment setters

--- src/models/Payment.py
+++ src/models/Payment.py
@@ -30,11 +30,22 @@ class Payment:
     def value(self) -> float:
         return self._value
 
+    @value.setter
+    def value(self, value: float) -> None:
+        self._value = value
+
     @property
     def payment_date(self) -> date:
         return self._payment_date
+
+    @payment_date.setter
+    def payment_date(self, payment_date: date) -> None:
+        self._payment_date = payment_date
 
     @property
     def reference_date(self) -> date:
         return self._reference_date
 
+    @reference_date.setter
+    def reference_date(self, reference_date: date) -> None:
+        self._reference_date = reference_date
--- tests/test_models/test_payment.py
+++ tests/test_models/test_payment.py
@@ -35,4 +35,23 @@ class TestPayment(unittest.TestCase):
         self.assertEqual(payment.payment_date, date(2021, 1, 1))
         self.assertEqual(payment.reference_date, date(2021, 1, 1))
 
+    def test_setters(self) -> None:
+        """
+        Testa os setters da classe Payment conferindo se
+        eles atribuem os valores corretos.
+        """
+        payment = Payment(
+            value=10.0,
+            payment_date=date(2021, 1, 1),
+            reference_date=date(2021, 1, 1),
+        )
+        payment.value = 20.0
+        payment.payment_date = date(2021, 1, 2)
+        payment.reference_date = date(2021, 1, 3)
+        self.assertEqual(payment.value, 20.0)
+        self.assertEqual(payment.payment_date, date(2021, 1, 2))
+        self.assertEqual(payment.reference_date, date(2021, 1, 3))
 
+
+if __name__ == "__main__":
+    unittest.main()

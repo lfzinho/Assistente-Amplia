ec8ae760084976539e78b5fb915b6346505cf59a
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-12-09 19:22:49 -0300

    style: add docstrings

--- src/models/Payment.py
+++ src/models/Payment.py
@@ -14,6 +14,43 @@ from datetime import date
 #         self.reference_date = reference_date
 
 
+# class Payment:
+#     """Classe para representar um pagamento"""
+#     def __init__(
+#         self,
+#         value: float,
+#         payment_date: date,
+#         reference_date: date,
+#     ) -> None:
+#         self._value = value
+#         self._payment_date = payment_date
+#         self._reference_date = reference_date
+
+#     @property
+#     def value(self) -> float:
+#         return self._value
+
+#     @value.setter
+#     def value(self, value: float) -> None:
+#         self._value = value
+
+#     @property
+#     def reference_date(self) -> date:
+#         return self._reference_date
+
+#     @reference_date.setter
+#     def reference_date(self, reference_date: date) -> None:
+#         self._reference_date = reference_date
+
+#     @property
+#     def payment_date(self) -> date:
+#         return self._payment_date
+
+#     @payment_date.setter
+#     def payment_date(self, payment_date: date) -> None:
+#         self._payment_date = payment_date
+
+
 class Payment:
     """Classe para representar um pagamento"""
     def __init__(
@@ -22,30 +59,118 @@ class Payment:
         payment_date: date,
         reference_date: date,
     ) -> None:
+        """
+        Construtor da classe Payment
+
+        Parâmetros:
+        -----------
+        value: float
+            Valor do pagamento
+        payment_date: date
+            Data do pagamento
+        reference_date: date
+            Data da aula à qual o pagamento se refere
+
+        Retorno:
+        --------
+        None
+        """
         self._value = value
         self._payment_date = payment_date
         self._reference_date = reference_date
 
     @property
     def value(self) -> float:
+        """
+        Getter para o valor do pagamento
+
+        Parâmetros:
+        -----------
+        None
+
+        Retorno:
+        --------
+        float
+            Valor do pagamento
+        """
         return self._value
 
     @value.setter
     def value(self, value: float) -> None:
+        """
+        Setter para o valor do pagamento
+
+        Parâmetros:
+        -----------
+        value: float
+            Valor do pagamento
+
+        Retorno:
+        --------
+        None
+        """
         self._value = value
 
     @property
-    def payment_date(self) -> date:
-        return self._payment_date
-
-    @payment_date.setter
-    def payment_date(self, payment_date: date) -> None:
-        self._payment_date = payment_date
-
-    @property
     def reference_date(self) -> date:
+        """
+        Getter para a data da aula à qual o pagamento se refere
+
+        Parâmetros:
+        -----------
+        None
+
+        Retorno:
+        --------
+        date
+            Data da aula à qual o pagamento se refere
+        """
         return self._reference_date
 
     @reference_date.setter
     def reference_date(self, reference_date: date) -> None:
+        """
+        Setter para a data da aula à qual o pagamento se refere
+
+        Parâmetros:
+        -----------
+        reference_date: date
+            Data da aula à qual o pagamento se refere
+
+        Retorno:
+        --------
+        None
+        """
         self._reference_date = reference_date
+
+    @property
+    def payment_date(self) -> date:
+        """
+        Getter para a data do pagamento
+
+        Parâmetros:
+        -----------
+        None
+
+        Retorno:
+        --------
+        date
+            Data do pagamento
+        """
+        return self._payment_date
+
+    @payment_date.setter
+    def payment_date(self, payment_date: date) -> None:
+        """
+        Setter para a data do pagamento
+
+        Parâmetros:
+        -----------
+        payment_date: date
+            Data do pagamento
+
+        Retorno:
+        --------
+        None
+        """
+        self._payment_date = payment_date

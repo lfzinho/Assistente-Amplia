84d93e26709a2b531797fb0f933f75f6ceab432f
viniciushedler <viniciushedler@gmail.com>
2023-12-09 20:58:54 -0300

    feature: test_administrator e Administrator. Por agora é uma classe muito simples

--- src/models/Administrator.py
+++ src/models/Administrator.py
@@ -1,5 +1,13 @@
 from datetime import date
 from typing import Literal, Optional, Type
 
-from src.models.Person import Person
+from src.models.Director import Director
 from src.models.PaymentMethod import PaymentMethod
+
+
+class Administrator(Director):
+    def __init__(self, name, email, cpf, address, payment_method,
+        birth_date, admission_date, occupation_area,
+        office_admission_date, office_exit_date=None, exit_date=None):
+        super().__init__(name, email, cpf, address, payment_method,
+        birth_date, admission_date, occupation_area, exit_date)
---
+++ tests/test_models/test_administrator.py
@@ -0,0 +1,32 @@
+import unittest
+from abc import ABC
+from datetime import date
+
+from src.models.Pix import Pix
+from src.models.Administrator import Administrator
+
+
+class TestAdministrator(unittest.TestCase):
+    """Classe que testa a classe Administrator"""
+    def test_constructor(self) -> None:
+        """
+        Testa o construtor da classe Administrator conferindo se os
+        atributos foram setados corretamente.
+        """
+        director = Administrator(
+            name='João',
+            email='joaosilva@gmail.com',
+            cpf='123.456.789-00',
+            address='Rua 1, 123',
+            payment_method=Pix(key='12345678900', type_='cpf'),
+            birth_date=date(year=1990, month=1, day=1),
+            admission_date=date(year=2021, month=1, day=1),
+            occupation_area='Administrativo Financeiro',
+            office_admission_date=date(year=2021, month=1, day=1),
+            office_exit_date=date(year=2022, month=1, day=1),
+            exit_date=date(year=2022, month=1, day=1),
+        )
+        self.assertIsInstance(director, Administrator)
+
+if __name__ == '__main__':
+    unittest.main()

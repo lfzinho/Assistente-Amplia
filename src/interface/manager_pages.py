65136a2a93e9fea083925148a0e75719bd363632
marcelo-souzaf <marceloamaral386@gmail.com>
2023-12-10 20:26:27 -0300

    style: padrão de aspas

--- src/interface/manager_pages.py
+++ src/interface/manager_pages.py
@@ -81,10 +81,10 @@ class ManagerPage(ABC):
 
     def show_table(self) -> None:
         """Shows the table of the managed elements on the page."""
-        st.subheader("Tabela de Elementos")
+        st.subheader('Tabela de Elementos')
         query = self.db_manager.get_all(self.db_collection)
         if query == {}:
-            st.write("Ainda não há nenhum elemento cadastrado.")
+            st.write('Ainda não há nenhum elemento cadastrado.')
         else:
             df = pd.DataFrame(query)
             st.dataframe(df.T)
@@ -116,121 +116,122 @@ class ManagerPage(ABC):
     def render(self) -> None:
         """Renders the page in the usual organization."""
         if (
-            "uid" in st.session_state
-            and self.auth.get_user_by_uid(st.session_state["uid"]) is not None
+            'uid' in st.session_state
+            and self.auth.get_user_by_uid(st.session_state['uid']) is not None
         ):
             st.title(self.title)
             st.write(self.description)
             self.show_table()
             self.show_form_tabs()
         else:
-            st.warning("Você precisa estar autenticado "
-                       "para acessar esta página.")
+            st.warning(
+                'Você precisa estar autenticado para acessar esta página.'
+            )
 
 
 class PersonPage(ManagerPage):
     def __init__(self) -> None:
         super().__init__(
-            title="Página de Beneficiário",
-            description="Gerencie os beneficiários do sistema.",
+            title='Página de Beneficiário',
+            description='Gerencie os beneficiários do sistema.',
             creation_form=PersonCreationForm(),
             update_form=PersonUpdateForm(),
             deletion_form=PersonDeletionForm(),
-            db_collection="person"
+            db_collection='person'
         )
 
 
 class BeneficiaryPage(ManagerPage):
     def __init__(self) -> None:
         super().__init__(
-            title="Página de Beneficiário",
-            description="Gerencie os beneficiários do sistema.",
+            title='Página de Beneficiário',
+            description='Gerencie os beneficiários do sistema.',
             creation_form=BeneficiaryCreationForm(),
             update_form=BeneficiaryUpdateForm(),
             deletion_form=BeneficiaryDeletionForm(),
-            db_collection="beneficiary"
+            db_collection='beneficiary'
         )
 
 
 class StudentPage(ManagerPage):
     def __init__(self) -> None:
         super().__init__(
-            title="Página de Estudante",
-            description="Gerencie os estudantes do sistema.",
+            title='Página de Estudante',
+            description='Gerencie os estudantes do sistema.',
             creation_form=StudentCreationForm(),
             update_form=StudentUpdateForm(),
             deletion_form=StudentDeletionForm(),
-            db_collection="student"
+            db_collection='student'
         )
 
 
 class TeacherPage(ManagerPage):
     def __init__(self) -> None:
         super().__init__(
-            title="Página de Professor",
-            description="Gerencie os professores do sistema.",
+            title='Página de Professor',
+            description='Gerencie os professores do sistema.',
             creation_form=TeacherCreationForm(),
             update_form=TeacherUpdateForm(),
             deletion_form=TeacherDeletionForm(),
-            db_collection="teacher"
+            db_collection='teacher'
         )
 
 
 class AnalystPage(ManagerPage):
     def __init__(self) -> None:
         super().__init__(
-            title="Página de Analista",
-            description="Gerencie os analistas do sistema.",
+            title='Página de Analista',
+            description='Gerencie os analistas do sistema.',
             creation_form=AnalystCreationForm(),
             update_form=AnalystUpdateForm(),
             deletion_form=AnalystDeletionForm(),
-            db_collection="analyst"
+            db_collection='analyst'
         )
 
 
 class DirectorPage(ManagerPage):
     def __init__(self) -> None:
         super().__init__(
-            title="Página de Diretor",
-            description="Gerencie os diretores do sistema.",
+            title='Página de Diretor',
+            description='Gerencie os diretores do sistema.',
             creation_form=DirectorCreationForm(),
             update_form=DirectorUpdateForm(),
             deletion_form=DirectorDeletionForm(),
-            db_collection="director"
+            db_collection='director'
         )
 
 
 class AdministratorPage(ManagerPage):
     def __init__(self) -> None:
         super().__init__(
-            title="Página de Administrador",
-            description="Gerencie os administradores do sistema.",
+            title='Página de Administrador',
+            description='Gerencie os administradores do sistema.',
             creation_form=AdministratorCreationForm(),
             update_form=AdministratorUpdateForm(),
             deletion_form=AdministratorDeletionForm(),
-            db_collection="administrator"
+            db_collection='administrator'
         )
 
 
 class PresenceListPage(ManagerPage):
     def __init__(self) -> None:
         super().__init__(
-            title="Página de Lista de Presença",
-            description="Gerencie as listas de presença do sistema.",
+            title='Página de Lista de Presença',
+            description='Gerencie as listas de presença do sistema.',
             creation_form=PresenceListCreationForm(),
             update_form=PresenceListUpdateForm(),
             deletion_form=PresenceListDeletionForm(),
-            db_collection="presence_list"
+            db_collection='presence_list'
         )
 
 
 class PaymentPage(ManagerPage):
     def __init__(self) -> None:
         super().__init__(
-            title="Página de Pagamento",
-            description="Gerencie os pagamentos do sistema.",
+            title='Página de Pagamento',
+            description='Gerencie os pagamentos do sistema.',
             creation_form=PaymentCreationForm(),
             update_form=PaymentUpdateForm(),
             deletion_form=PaymentDeletionForm(),
-            db_collection="payment"
+            db_collection='payment'
         )

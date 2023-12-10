01126e62290aac79e0e0c1f6ef17fb4125d91227
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-12-09 21:46:48 -0300

    style: comments for tdd

--- src/models/EventManager.py
+++ src/models/EventManager.py
@@ -8,6 +8,32 @@ from src.models.EventListener import EventListener
 #         self.listeners = []
 
 
+# class EventManager:
+#     """Classe responsável por gerenciar o padrão de projeto Observer."""
+
+#     def __init__(self) -> None:
+#         self.listeners = []
+
+#     def add_listener(self, listener: EventListener) -> None:
+#         """Método responsável por adicionar um listener."""
+#         self.listeners.append(listener)
+
+
+# class EventManager:
+#     """Classe responsável por gerenciar o padrão de projeto Observer."""
+
+#     def __init__(self) -> None:
+#         self.listeners = []
+
+#     def add_listener(self, listener: EventListener) -> None:
+#         """Método responsável por adicionar um listener."""
+#         self.listeners.append(listener)
+
+#     def remove_listener(self, listener: EventListener) -> None:
+#         """Método responsável por remover um listener."""
+#         self.listeners.remove(listener)
+
+
 class EventManager:
     """Classe responsável por gerenciar o padrão de projeto Observer."""
 

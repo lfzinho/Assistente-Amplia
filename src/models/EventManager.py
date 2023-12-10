e9a6d6f2ccff74eb976afb7e4f03ee1b635b605d
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-12-10 18:11:15 -0300

    feat: make EventManager abstract

--- src/models/EventManager.py
+++ src/models/EventManager.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 from src.models.EventListener import EventListener
 
 
@@ -34,7 +36,27 @@ from src.models.EventListener import EventListener
 #         self.listeners.remove(listener)
 
 
-class EventManager:
+# class EventManager:
+#     """Classe responsável por gerenciar o padrão de projeto Observer."""
+
+#     def __init__(self) -> None:
+#         self.listeners = []
+
+#     def add_listener(self, listener: EventListener) -> None:
+#         """Método responsável por adicionar um listener."""
+#         self.listeners.append(listener)
+
+#     def remove_listener(self, listener: EventListener) -> None:
+#         """Método responsável por remover um listener."""
+#         self.listeners.remove(listener)
+
+#     def notify(self, event: str) -> None:
+#         """Método responsável por notificar os listeners."""
+#         for listener in self.listeners:
+#             listener.update(event)
+
+
+class EventManager(ABC):
     """Classe responsável por gerenciar o padrão de projeto Observer."""
 
     def __init__(self) -> None:

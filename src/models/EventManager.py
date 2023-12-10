14ad891c83bc49930df23df05cdfdf82800786f5
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-12-09 21:42:01 -0300

    feat: add add_listener() to EventManager

--- src/models/EventManager.py
+++ src/models/EventManager.py
@@ -1,7 +1,20 @@
+from src.models.EventListener import EventListener
+
+
+# class EventManager:
+#     """Classe responsável por gerenciar o padrão de projeto Observer."""
+
+#     def __init__(self) -> None:
+#         self.listeners = []
+
+
 class EventManager:
     """Classe responsável por gerenciar o padrão de projeto Observer."""
 
     def __init__(self) -> None:
         self.listeners = []
 
+    def add_listener(self, listener: EventListener) -> None:
+        """Método responsável por adicionar um listener."""
+        self.listeners.append(listener)
 
--- tests/test_models/test_event_manager.py
+++ tests/test_models/test_event_manager.py
@@ -2,6 +2,7 @@ import unittest
 
 from . import init_path
 from src.models.EventManager import EventManager
+from src.models.PaymentEventListener import PaymentEventListener
 
 
 class TestEventManager(unittest.TestCase):
@@ -15,6 +16,15 @@ class TestEventManager(unittest.TestCase):
         self.assertIsInstance(event_manager, EventManager)
         self.assertEqual(event_manager.listeners, [])
 
+    def test_add_listener(self) -> None:
+        """
+        Testa o método add_listener da classe EventManager conferindo se os
+        atributos foram setados corretamente.
+        """
+        event_manager = EventManager()
+        payment_event_listener = PaymentEventListener()
+        event_manager.add_listener(payment_event_listener)
+        self.assertEqual(event_manager.listeners, [payment_event_listener])
 
 if __name__ == '__main__':
     unittest.main()

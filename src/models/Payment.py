c70a11e16a571b1d32002718b944a26ea6aef4c5
Gianlucca Devigili <gianluccadevigili@gmail.com>
2023-12-10 22:12:31 -0300

    feat: add database attributes

--- src/models/Payment.py
+++ src/models/Payment.py
@@ -523,13 +523,130 @@ from src.models.PaymentEventListener import PaymentEventListener
 #         self._payment_date = payment_date
 
 
+# class Payment(EventManager):
+#     """Classe para representar um pagamento"""
+#     def __init__(
+#         self,
+#         value: float,
+#         payment_date: date,
+#         reference_date: date,
+#     ) -> None:
+#         """
+#         Construtor da classe Payment.
+
+#         Parâmetros
+#         ----------
+#         value: float
+#             Valor do pagamento.
+#         payment_date: date
+#             Data do pagamento.
+#         reference_date: date
+#             Data da aula à qual o pagamento se refere.
+#         """
+#         super().__init__()
+#         self._value = value
+#         self._payment_date = payment_date
+#         self._reference_date = reference_date
+#         # adiciona um listener para o evento de alteração do valor do pagamento
+#         self.add_listener(PaymentEventListener())
+
+#     def notify(self, event: str) -> None:
+#         """Método responsável por notificar os listeners."""
+#         for listener in self.listeners:
+#             listener.update(event)
+
+#     @property
+#     def value(self) -> float:
+#         """
+#         Getter para o valor do pagamento.
+
+#         Retorna
+#         -------
+#         float
+#             Valor do pagamento.
+#         """
+#         return self._value
+
+#     @value.setter
+#     def value(self, value: float) -> None:
+#         """
+#         Setter para o valor do pagamento.
+
+#         Parâmetros
+#         ----------
+#         value: float
+#             Valor do pagamento.
+#         """
+#         if value < 0:
+#             raise ValueError("O valor do pagamento não pode ser negativo")
+#         self.notify("Valor de pagamento modificado")
+#         self._value = value
+
+#     @property
+#     def reference_date(self) -> date:
+#         """
+#         Getter para a data da aula à qual o pagamento se refere.
+
+#         Retorna
+#         -------
+#         datetime.date
+#             Data da aula à qual o pagamento se refere.
+#         """
+#         return self._reference_date
+
+#     @reference_date.setter
+#     def reference_date(self, reference_date: date) -> None:
+#         """
+#         Setter para a data da aula à qual o pagamento se refere.
+
+#         Parâmetros
+#         ----------
+#         reference_date: datetime.date
+#             Data da aula à qual o pagamento se refere.
+#         """
+#         self.notify("Data de referência de pagamento modificada")
+#         self._reference_date = reference_date
+
+#     @property
+#     def payment_date(self) -> date:
+#         """
+#         Getter para a data do pagamento.
+
+#         Retorna
+#         -------
+#         datetime.date
+#             Data do pagamento.
+#         """
+#         return self._payment_date
+
+#     @payment_date.setter
+#     def payment_date(self, payment_date: date) -> None:
+#         """
+#         Setter para a data do pagamento.
+
+#         Parâmetros
+#         ----------
+#         payment_date: datetime.date
+#             Data do pagamento.
+#         """
+#         if payment_date < self.reference_date:
+#             raise ValueError(
+#                 "A data do pagamento não pode ser anterior à data da aula"
+#             )
+#         self.notify("Data de pagamento modificada")
+#         self._payment_date = payment_date
+
 class Payment(EventManager):
     """Classe para representar um pagamento"""
     def __init__(
         self,
+        id_payment: str,
         value: float,
         payment_date: date,
         reference_date: date,
+        id_administrator: str,
+        id_beneficiary: str,
+        paid: bool = False,
     ) -> None:
         """
         Construtor da classe Payment.
@@ -544,9 +661,13 @@ class Payment(EventManager):
             Data da aula à qual o pagamento se refere.
         """
         super().__init__()
+        self._id_payment = id_payment
         self._value = value
         self._payment_date = payment_date
         self._reference_date = reference_date
+        self._id_administrator = id_administrator
+        self._id_beneficiary = id_beneficiary
+        self._paid = paid
         # adiciona um listener para o evento de alteração do valor do pagamento
         self.add_listener(PaymentEventListener())
 
@@ -554,6 +675,31 @@ class Payment(EventManager):
         """Método responsável por notificar os listeners."""
         for listener in self.listeners:
             listener.update(event)
+
+    @property
+    def id_payment(self) -> str:
+        """
+        Getter para o ID do pagamento.
+
+        Retorna
+        -------
+        str
+            ID do pagamento.
+        """
+        return self._id_payment
+
+    @id_payment.setter
+    def id_payment(self, id_payment: str) -> None:
+        """
+        Setter para o ID do pagamento.
+
+        Parâmetros
+        ----------
+        id_payment: str
+            ID do pagamento.
+        """
+        self.notify(f"ID de pagamento modificado para pagamento {self.id_payment}")
+        self._id_payment = id_payment
 
     @property
     def value(self) -> float:
@@ -579,7 +725,7 @@ class Payment(EventManager):
         """
         if value < 0:
             raise ValueError("O valor do pagamento não pode ser negativo")
-        self.notify("Valor de pagamento modificado")
+        self.notify(f"Valor de pagamento modificado para pagamento {self.id_payment}. Novo valor: {value}")
         self._value = value
 
     @property
@@ -604,7 +750,7 @@ class Payment(EventManager):
         reference_date: datetime.date
             Data da aula à qual o pagamento se refere.
         """
-        self.notify("Data de referência de pagamento modificada")
+        self.notify(f"Data de referência de pagamento modificada para pagamento {self.id_payment}. Nova data: {reference_date}")
         self._reference_date = reference_date
 
     @property
@@ -633,5 +779,80 @@ class Payment(EventManager):
             raise ValueError(
                 "A data do pagamento não pode ser anterior à data da aula"
             )
-        self.notify("Data de pagamento modificada")
+        self.notify(f"Data de pagamento modificada para o pagamento {self.id_payment}. Nova data: {payment_date}")
         self._payment_date = payment_date
+
+    @property
+    def id_administrator(self) -> int:
+        """
+        Getter para o ID do administrador.
+
+        Retorna
+        -------
+        int
+            ID do administrador.
+        """
+        return self._id_administrator
+
+    @id_administrator.setter
+    def id_administrator(self, id_administrator: int) -> None:
+        """
+        Setter para o ID do administrador.
+
+        Parâmetros
+        ----------
+        id_administrator: int
+            ID do administrador.
+        """
+        self.notify(f"ID do administrador modificado para pagamento {self.id_payment}. Novo ID: {id_administrator}")
+        self._id_administrator = id_administrator
+
+    @property
+    def id_beneficiary(self) -> str:
+        """
+        Getter para o ID do beneficiário.
+
+        Retorna
+        -------
+        str
+            ID do beneficiário.
+        """
+        return self._id_beneficiary
+
+    @id_beneficiary.setter
+    def id_beneficiary(self, id_beneficiary: str) -> None:
+        """
+        Setter para o ID do beneficiário.
+
+        Parâmetros
+        ----------
+        id_beneficiary: str
+            ID do beneficiário.
+        """
+        self.notify(f"ID do beneficiário modificado para pagamento {self.id_payment}. Novo ID: {id_beneficiary}")
+        self._id_beneficiary = id_beneficiary
+
+    @property
+    def paid(self) -> bool:
+        """
+        Getter para o status de pagamento.
+
+        Retorna
+        -------
+        bool
+            Status de pagamento.
+        """
+        return self._paid
+
+    @paid.setter
+    def paid(self, paid: bool) -> None:
+        """
+        Setter para o status de pagamento.
+
+        Parâmetros
+        ----------
+        paid: bool
+            Status de pagamento.
+        """
+        self.notify(f"Status de pagamento modificado para o pagamento {self.id_payment}. Novo status: {paid}")
+        self._paid = paid
--- tests/test_models/test_payment.py
+++ tests/test_models/test_payment.py
@@ -14,14 +14,22 @@ class TestPayment(unittest.TestCase):
         atributos foram definidos corretamente.
         """
         payment = Payment(
+            id_payment='1',
             value=10.0,
             payment_date=date(2021, 1, 1),
             reference_date=date(2021, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
         )
         self.assertIsInstance(payment, Payment)
+        self.assertEqual(payment.id_payment, '1')
         self.assertEqual(payment.value, 10.0)
         self.assertEqual(payment.payment_date, date(2021, 1, 1))
         self.assertEqual(payment.reference_date, date(2021, 1, 1))
+        self.assertEqual(payment.id_administrator, '1')
+        self.assertEqual(payment.id_beneficiary, '1')
+        self.assertTrue(payment.paid)
 
     def test_getters(self) -> None:
         """
@@ -29,13 +37,21 @@ class TestPayment(unittest.TestCase):
         eles retornam os valores corretos.
         """
         payment = Payment(
+            id_payment='1',
             value=10.0,
             payment_date=date(2021, 1, 1),
             reference_date=date(2021, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
         )
+        self.assertEqual(payment.id_payment, '1')
         self.assertEqual(payment.value, 10.0)
         self.assertEqual(payment.payment_date, date(2021, 1, 1))
         self.assertEqual(payment.reference_date, date(2021, 1, 1))
+        self.assertEqual(payment.id_administrator, '1')
+        self.assertEqual(payment.id_beneficiary, '1')
+        self.assertTrue(payment.paid)
 
     def test_setters(self) -> None:
         """
@@ -43,16 +59,28 @@ class TestPayment(unittest.TestCase):
         eles atribuem os valores corretos.
         """
         payment = Payment(
+            id_payment='1',
             value=10.0,
             payment_date=date(2021, 1, 1),
             reference_date=date(2021, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
         )
+        payment.id_payment = '2'
         payment.value = 20.0
         payment.payment_date = date(2021, 1, 2)
         payment.reference_date = date(2021, 1, 3)
+        payment.id_administrator = '2'
+        payment.id_beneficiary = '2'
+        payment.paid = False
+        self.assertEqual(payment.id_payment, '2')
         self.assertEqual(payment.value, 20.0)
         self.assertEqual(payment.payment_date, date(2021, 1, 2))
         self.assertEqual(payment.reference_date, date(2021, 1, 3))
+        self.assertEqual(payment.id_administrator, '2')
+        self.assertEqual(payment.id_beneficiary, '2')
+        self.assertFalse(payment.paid)
 
     def test_payment_date_setter_limitation(self) -> None:
         """
@@ -61,13 +89,17 @@ class TestPayment(unittest.TestCase):
         menor que o atributo reference_date.
         """
         payment = Payment(
+            id_payment='1',
             value=10.0,
-            payment_date=date(2021, 1, 2),
-            reference_date=date(2021, 1, 2),
+            payment_date=date(2021, 1, 3),
+            reference_date=date(2021, 1, 3),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
         )
         with self.assertRaises(ValueError):
-            payment.payment_date = date(2021, 1, 1)
-        self.assertEqual(payment.payment_date, date(2021, 1, 2))
+            payment.payment_date = date(2021, 1, 2)
+        self.assertEqual(payment.payment_date, date(2021, 1, 3))
 
     def test_value_setter_limitation(self) -> None:
         """
@@ -75,9 +107,13 @@ class TestPayment(unittest.TestCase):
         permite que o atributo value seja menor que zero.
         """
         payment = Payment(
+            id_payment='1',
             value=10.0,
-            payment_date=date(2021, 1, 2),
-            reference_date=date(2021, 1, 2),
+            payment_date=date(2021, 1, 1),
+            reference_date=date(2021, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
         )
         with self.assertRaises(ValueError):
             payment.value = -1.0
@@ -96,18 +132,40 @@ class TestPayment(unittest.TestCase):
         no construtor da classe Payment.
         """
         payment = Payment(
+            id_payment='1',
             value=10.0,
-            payment_date=date(2021, 1, 2),
-            reference_date=date(2021, 1, 2),
+            payment_date=date(2021, 1, 1),
+            reference_date=date(2021, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
         )
         self.assertTrue(any(isinstance(listener, PaymentEventListener) for listener in payment.listeners))
 
     @patch.object(Payment, 'notify')
-    def test_value_setter_calls_notify(self, mock_notify):
+    def test_id_setter_calls_notify(self, mock_notify):
         payment = Payment(
+            id_payment='1',
             value=10.0,
             payment_date=date(2021, 1, 2),
-            reference_date=date(2021, 1, 2),
+            reference_date=date(2020, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
+        )
+        payment.id = '2'
+        self.assertTrue(mock_notify.called)
+
+    @patch.object(Payment, 'notify')
+    def test_value_setter_calls_notify(self, mock_notify):
+        payment = Payment(
+            id_payment='1',
+            value=10.0,
+            payment_date=date(2021, 1, 1),
+            reference_date=date(2021, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
         )
         payment.value = 10.0
         self.assertTrue(mock_notify.called)
@@ -115,9 +173,13 @@ class TestPayment(unittest.TestCase):
     @patch.object(Payment, 'notify')
     def test_payment_date_setter_calls_notify(self, mock_notify):
         payment = Payment(
+            id_payment='1',
             value=10.0,
-            payment_date=date(2021, 1, 2),
-            reference_date=date(2021, 1, 2),
+            payment_date=date(2021, 1, 1),
+            reference_date=date(2021, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
         )
         payment.payment_date = date(2021, 1, 2)
         self.assertTrue(mock_notify.called)
@@ -125,19 +187,69 @@ class TestPayment(unittest.TestCase):
     @patch.object(Payment, 'notify')
     def test_reference_date_setter_calls_notify(self, mock_notify):
         payment = Payment(
+            id_payment='1',
             value=10.0,
-            payment_date=date(2021, 1, 2),
-            reference_date=date(2021, 1, 2),
+            payment_date=date(2021, 1, 1),
+            reference_date=date(2021, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
         )
         payment.reference_date = date(2021, 1, 2)
+        self.assertTrue(mock_notify.called)
+
+    @patch.object(Payment, 'notify')
+    def test_id_administrator_setter_calls_notify(self, mock_notify):
+        payment = Payment(
+            id_payment='1',
+            value=10.0,
+            payment_date=date(2021, 1, 1),
+            reference_date=date(2020, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
+        )
+        payment.id_administrator = '2'
+        self.assertTrue(mock_notify.called)
+
+    @patch.object(Payment, 'notify')
+    def test_id_beneficiary_setter_calls_notify(self, mock_notify):
+        payment = Payment(
+            id_payment='1',
+            value=10.0,
+            payment_date=date(2021, 1, 1),
+            reference_date=date(2020, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
+        )
+        payment.id_beneficiary = '2'
+        self.assertTrue(mock_notify.called)
+
+    @patch.object(Payment, 'notify')
+    def test_paid_setter_calls_notify(self, mock_notify):
+        payment = Payment(
+            id_payment='1',
+            value=10.0,
+            payment_date=date(2021, 1, 2),
+            reference_date=date(2020, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
+        )
+        payment.paid = False
         self.assertTrue(mock_notify.called)
 
     @patch.object(PaymentEventListener, 'update')
     def test_payment_event_listener_update_is_called(self, mock_update):
         payment = Payment(
+            id_payment='1',
             value=10.0,
-            payment_date=date(2021, 1, 2),
-            reference_date=date(2021, 1, 2),
+            payment_date=date(2021, 1, 1),
+            reference_date=date(2021, 1, 1),
+            id_administrator='1',
+            id_beneficiary='1',
+            paid=True,
         )
         payment.value = 20.0
         self.assertTrue(mock_update.called)
